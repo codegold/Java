2337e8297030eb8b8c5601c983d40542ff027dae
«MaxRubshik» <rubshikmax@gmail.com>
2018-11-01 16:16:06 +0300

    New function.

--- IfDemo.java
+++ IfDemo.java
@@ -7,7 +7,12 @@ public static void main(String[]args){
     if  (a < b) System.out.println("a lower than b.");
     if (a == b) System.out.println("You'll don't ever see this!");
 
-    System.out.println();
+    int count;
+
+    for (count = 0; count < 5; count = count+1)
+        System.out.println("Mississipi is " + count);
+
+    System.out.println("It over for now.");
 
 
 }
